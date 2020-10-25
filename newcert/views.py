6faaf098de1c07373d4cc4e4e20ef09cd9750482
bcv <bcv@bcvm.unixindia.com>
2020-10-25 22:39:18 +0000

    Basic new pages

--- newcert/views.py
+++ newcert/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 
 def newcert(request):
-        return(render(request,'newcert/newcertform.html'))
+    return(render(request,'newcert/newcertform.html',{'exttitle':'New Certificate Page'}))
 
 
 # Create your views here.
